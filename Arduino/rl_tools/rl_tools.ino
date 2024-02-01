4efd229e12b6a05f62a9676a368bece78ad9e420
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-02-01 16:00:18 -0500

    benchmark

--- Arduino/rl_tools/rl_tools.ino
+++ Arduino/rl_tools/rl_tools.ino
@@ -1,3 +1,17 @@
+/*
+Memory strategy:
+Teensy 4.1 has 3 main memory regions:
+RAM1 _:(tightly coupled, fastest) 512kb: static allocations (global variables)
+RAM2 DMAMEM: (relatively tightly coupled) 512kb: dynamic memory allocations
+PSRAM EXTMEM: 16mb: global variables decorated with EXTMEM
+
+Strategy: 
+RAM1: code, otherwise nothing
+RAM2: actor, actor_buffers
+EXTMEM: off_policy_runner/replay_buffer
+*/
+
+
 #undef abs
 #undef PI
 #undef INPUT
@@ -7,6 +21,7 @@
 #define TRAIN
 #ifdef TRAIN
 #define RL_TOOLS_DEPLOYMENT_ARDUINO
+#define BENCHMARK
 // #define RL_TOOLS_DISABLE_EVALUATION
 #include </home/jonas/rl_tools/src/rl/environments/pendulum/sac/arm/training2.h>
 #else
@@ -14,126 +29,124 @@
 #include <rl_tools/nn/layers/dense/operations_arm/dsp.h>
 #include <rl_tools/nn/layers/dense/operations_arm/opt.h>
 #include <rl_tools/nn_models/mlp/operations_generic.h>
-#include <data/test_rl_tools_nn_models_mlp_evaluation.h>
-#include <data/test_rl_tools_nn_models_mlp_persist_code.h>
+// #include <data/test_rl_tools_nn_models_mlp_evaluation.h>
+// #include <data/test_rl_tools_nn_models_mlp_persist_code.h>
 namespace rlt = rl_tools;
 #endif
 
-#ifdef TRAIN
-// void main_train(){
-//   Serial.print("pre train\n");
-//   unsigned long start = millis();
-//   train();
-//   auto end = millis();
-//   Serial.printf("post train, took: %lu\n", end - start);
-// #ifndef RL_TOOLS_DISABLE_EVALUATION
-//   for(int i = 0; i < LOOP_CONFIG::PARAMETERS::N_EVALUATIONS; i++){
-//     Serial.printf("Evaluation[%d]: %f \n", i, ts.evaluation_returns[i]);
-//   }
-// #endif
+using CONFIG = LOOP_CORE_CONFIG;
+
+
+// // core state
+// template<typename SPEC>
+// using CONTAINER_TYPE = typename CONFIG::CONTAINER_TYPE_TAG::template type<SPEC>;
+// typename CONFIG::NN::OPTIMIZER actor_optimizer, critic_optimizers[2];
+// typename CONFIG::RNG rng;
+// typename CONFIG::UI ui;
+// EXTMEM rlt::rl::components::OffPolicyRunner<typename CONFIG::OFF_POLICY_RUNNER_SPEC> off_policy_runner;
+// typename CONFIG::ENVIRONMENT envs[decltype(off_policy_runner)::N_ENVIRONMENTS];
+// EXTMEM typename CONFIG::ACTOR_CRITIC_TYPE actor_critic;
+// typename CONFIG::NN::ACTOR_TYPE::template Buffer<1> actor_deterministic_evaluation_buffers;
+// rlt::rl::components::off_policy_runner::Batch<rlt::rl::components::off_policy_runner::BatchSpecification<typename decltype(off_policy_runner)::SPEC, CONFIG::ACTOR_CRITIC_TYPE::SPEC::PARAMETERS::CRITIC_BATCH_SIZE>> critic_batch;
+// EXTMEM rlt::rl::algorithms::sac::CriticTrainingBuffers<typename CONFIG::ACTOR_CRITIC_SPEC> critic_training_buffers;
+// CONTAINER_TYPE<rlt::matrix::Specification<T, TI, CONFIG::PARAMETERS::SAC_PARAMETERS::CRITIC_BATCH_SIZE, CONFIG::ENVIRONMENT::ACTION_DIM>> action_noise_critic[2];
+// CONTAINER_TYPE<rlt::matrix::Specification<typename CONFIG::T, TI, 1, CONFIG::ENVIRONMENT::OBSERVATION_DIM>> observations_mean, observations_std;
+// EXTMEM typename CONFIG::NN::CRITIC_TYPE::template Buffer<CONFIG::ACTOR_CRITIC_TYPE::SPEC::PARAMETERS::CRITIC_BATCH_SIZE> critic_buffers[2];
+// rlt::rl::components::off_policy_runner::Batch<rlt::rl::components::off_policy_runner::BatchSpecification<typename decltype(off_policy_runner)::SPEC, CONFIG::ACTOR_CRITIC_TYPE::SPEC::PARAMETERS::ACTOR_BATCH_SIZE>> actor_batch;
+// rlt::rl::algorithms::sac::ActorTrainingBuffers<typename CONFIG::ACTOR_CRITIC_TYPE::SPEC> actor_training_buffers;
+// CONTAINER_TYPE<rlt::matrix::Specification<T, TI, CONFIG::PARAMETERS::SAC_PARAMETERS::CRITIC_BATCH_SIZE, CONFIG::ENVIRONMENT::ACTION_DIM>> action_noise_actor;
+// EXTMEM typename CONFIG::NN::ACTOR_TYPE::template Buffer<CONFIG::ACTOR_CRITIC_TYPE::SPEC::PARAMETERS::ACTOR_BATCH_SIZE> actor_buffers[2];
+// typename CONFIG::NN::ACTOR_TYPE::template Buffer<CONFIG::OFF_POLICY_RUNNER_SPEC::N_ENVIRONMENTS> actor_buffers_eval;
+
+// // evaluation state
+// rlt::rl::utils::evaluation::Result<T, TI, LOOP_EVAL_CONFIG::PARAMETERS::NUM_EVALUATION_EPISODES> evaluation_results[LOOP_EVAL_CONFIG::PARAMETERS::N_EVALUATIONS];
+// typename CONFIG::RNG rng_eval;
+// typename CONFIG::ENVIRONMENT_EVALUATION env_eval;
+
+// DEVICE device;
+// TI step;
+// TI seed = 0;
+// void setup(){
+//   Serial.begin(9600);
+  
+//   rlt::init(device);
+//   rlt::init(device, device.logger);
+
+//   // dynamically allocate actor_critic (for critic_1 and critic_2, others are static and hence no-op)
+//   rlt::malloc(device, actor_critic);
+//   rlt::malloc(device, actor_deterministic_evaluation_buffers);
+//   rlt::malloc(device, actor_buffers[0]);
+//   rlt::malloc(device, actor_buffers[1]);
+//   rlt::malloc(device, actor_buffers_eval);
+
+//   rng = rlt::random::default_engine(typename DEVICE::SPEC::RANDOM{}, seed);
+//   rng_eval = rlt::random::default_engine(typename DEVICE::SPEC::RANDOM{}, seed);
+
+//   rlt::init(device, actor_critic, rng);
+  
+//   rlt::init(device, envs[0], ui);
+//   rlt::init(device, env_eval, ui);
+//   rlt::init(device, off_policy_runner, envs);
+  
+
+//   off_policy_runner.parameters = rlt::rl::components::off_policy_runner::default_parameters<T>;
+
+
+//   step = 0;
 // }
-// #else
-// void main_test_timing(){
-//   unsigned long start = millis();
-//   int current_interval = 0;
-//   int interval = 10000;
-//   while(true){
-//     unsigned long current = millis();
-//     if(current_interval * interval < current - start){
-//       current_interval++;
-//       Serial.printf("Elapsed: %lums\n", current - start);
-//     }
+
+// void loop(){
+//   Serial.println(String("Step: ") + step);
+//   rlt::set_step(device, device.logger, step);
+//   bool finished = false;
+//   TI evaluation_index = step / LOOP_EVAL_CONFIG::PARAMETERS::EVALUATION_INTERVAL;
+//   if(step % LOOP_EVAL_CONFIG::PARAMETERS::EVALUATION_INTERVAL == 0 && evaluation_index < LOOP_EVAL_CONFIG::PARAMETERS::N_EVALUATIONS){
+//     auto result = rlt::evaluate(device, env_eval, ui, actor_critic.actor, rlt::rl::utils::evaluation::Specification<LOOP_EVAL_CONFIG::PARAMETERS::NUM_EVALUATION_EPISODES, CONFIG::PARAMETERS::ENVIRONMENT_STEP_LIMIT>(), observations_mean, observations_std, actor_deterministic_evaluation_buffers, rng_eval, false);
+//     rlt::log(device, device.logger, "Step: ", step, " Mean return: ", result.returns_mean);
+//     rlt::add_scalar(device, device.logger, "evaluation/return/mean", result.returns_mean);
+//     rlt::add_scalar(device, device.logger, "evaluation/return/std", result.returns_std);
+//     evaluation_results[evaluation_index] = result;
 //   }
-// }
-
-// template <typename DEVICE>
-// void main_evaluate_correctness(){
-
-//   DEVICE device;
-//   using T = float;
-//   using TI = typename DEVICE::index_t;
-//   int iterations = 1000;
-//   constexpr TI BATCH_SIZE = input::CONTAINER_SPEC::ROWS;
-//   decltype(mlp_1::mlp)::template Buffers<BATCH_SIZE, rlt::MatrixDynamicTag> buffers;
-
-//   rlt::MatrixDynamic<rlt::matrix::Specification<T, TI, BATCH_SIZE, decltype(mlp_1::mlp)::OUTPUT_DIM>> output;
-//   rlt::malloc(device, buffers);
-//   rlt::malloc(device, output);
-//   for(int iteration_i = 0; iteration_i < iterations; iteration_i++){
-//     rlt::evaluate(device, mlp_1::mlp, input::container, output);
-//   }
-//   for(int batch_i = 0; batch_i < BATCH_SIZE; batch_i++){
-//     for(int col_i = 0; col_i < decltype(mlp_1::mlp)::OUTPUT_DIM; col_i++){
-//         Serial.printf("%+5.5f ", get(output, batch_i, col_i));
-//     }
-//     Serial.printf("\n");
-//   }
-//   rlt::free(device, buffers);
-//   rlt::free(device, output);
-
-// }
-
-// template <typename DEVICE>
-// void main_evaluate_benchmark(){
-//   DEVICE device;
-//   int num_runs = 10000;
-//   decltype(mlp_1::mlp)::template Buffers<1, rlt::MatrixDynamicTag> buffers;
-//   using T = float;
-//   using TI = typename DEVICE::index_t;
-//   rlt::MatrixDynamic<rlt::matrix::Specification<T, TI, 1, decltype(mlp_1::mlp)::OUTPUT_DIM>> output;
-//   rlt::malloc(device, buffers);
-//   rlt::malloc(device, output);
-//   auto input_sample = rlt::view(device, input::container, rlt::matrix::ViewSpec<1, mlp_1::SPEC::INPUT_DIM>{}, 0, 0);
-//   while(true){
-//     unsigned long start = millis();
-//     for(int iteration_i = 0; iteration_i < num_runs; iteration_i++){
-//       rlt::evaluate(device, mlp_1::mlp, input_sample, output);
-//     }
-//     unsigned long end = millis();
-//     Serial.printf("Time taken: %lu ms\n", end - start);
-//     Serial.printf("Frequency: %f Hz\n", num_runs / ((end - start) / 1000.0));
-//       for(int batch_i = 0; batch_i < 1; batch_i++){
-//         for(int col_i = 0; col_i < decltype(mlp_1::mlp)::OUTPUT_DIM; col_i++){
-//             Serial.printf("%+5.5f ", get(output, batch_i, col_i));
-//         }
-//         Serial.printf("\n");
+//   rlt::step(device, off_policy_runner, actor_critic.actor, actor_buffers_eval, rng);
+//   if(step > CONFIG::PARAMETERS::N_WARMUP_STEPS){
+//       for(int critic_i = 0; critic_i < 2; critic_i++){
+//           rlt::gather_batch(device, off_policy_runner, critic_batch, rng);
+//           rlt::randn(device, action_noise_critic[critic_i], rng);
+//           rlt::train_critic(device, actor_critic, critic_i == 0 ? actor_critic.critic_1 : actor_critic.critic_2, critic_batch, critic_optimizers[critic_i], actor_buffers[critic_i], critic_buffers[critic_i], critic_training_buffers, action_noise_critic[critic_i]);
+//       }
+//       if(step % 1 == 0){
+//           {
+//               rlt::gather_batch(device, off_policy_runner, actor_batch, rng);
+//               rlt::randn(device, action_noise_actor, rng);
+//               rlt::train_actor(device, actor_critic, actor_batch, actor_optimizer, actor_buffers[0], critic_buffers[0], actor_training_buffers, action_noise_actor);
+//           }
+//           rlt::update_critic_targets(device, actor_critic);
 //       }
 //   }
-
-//   rlt::free(device, buffers);
-//   rlt::free(device, output);
+//   step++;
 // }
-#endif
 
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial.print("setup\n");
-
-}
+DEVICE device;
 EXTMEM LOOP_STATE ts;
-void train2(){
-    DEVICE device;
-
-    rlt::malloc(device, ts);
-    rlt::init(device, ts, 0);
-    print_sizes(device, ts);
-    while(!rlt::step(device, ts)){
-        // Serial.println(String("step: ") + ts.step);
-    }
-    rlt::free(device, ts);
+TI seed = 0;
+unsigned long start_time;
+void setup(){
+  Serial.begin(9600);
+  rlt::malloc(device, ts);
+  rlt::init(device, ts, 0);
+  print_sizes(device, ts);
+  start_time = millis();
 }
-
-void loop() {
-#ifdef TRAIN
-  // main_train();
-  train2();
-#else
-  using DEV_SPEC = rlt::devices::DefaultARMSpecification; 
-  using DEVICE = rlt::devices::arm::OPT<DEV_SPEC>;
-  main_evaluate_correctness<DEVICE>(); // disable this for "DSP" because it only supports BATCH_SIZE=1
-  main_evaluate_benchmark<DEVICE>();
-
-#endif
+void loop(){
+  unsigned long now = millis();
+  bool finished = rlt::step(device, ts);
+  if(ts.step % 100 == 0){
+    Serial.println(String("Elapsed[") + ts.step + String("]: ") + (now - start_time)/1000.0 + String("s"));
+  }
+  if(finished){
+      Serial.println(String("Finished in: ") + (now - start_time)/1000.0 + String("s"));
+      start_time = now;
+      rlt::init(device, ts, seed++);
+  }
 }
